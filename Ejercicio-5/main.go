65b1cfda1b3a9a5f11876ee2b992bb3736159b04
Matias De Stefano <matias.destefano@mercadolibre.com>
2021-12-16 17:17:03 -0300

    Ejercicio 5 Realizado

---
+++ Ejercicio-5/main.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	alumnos := []string{"Benjamin", "Nahuel", "Brenda", "Marcos", "Pedro","Axel", "Alez", "Dolores", "Federico", "Hernan", "Leandro", "Eduardo", "Duvraschka"}
+	alumnos = append(alumnos, "Gabriela")
+}
